e2a8203a27452e2d81f6bccb37d5f7dec5117b6d
Raakshitha <Raakshitha.pl@gmail.com>
2025-06-13 16:36:32 +0530

    Add files via upload

---
+++ count calculate OUTPUT.py
@@ -0,0 +1,12 @@
+Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:\Users\raaks\Downloads\python lab pro\count calculate 6 python.py =
+Enter the filename (e.g., sample.txt): lab 6 file run.txt
+
+Top 5 most frequent words in 'lab 6 file run.txt':
+i: 1 times
+like: 1 times
+to: 1 times
+draw: 1 times
+and: 1 times
